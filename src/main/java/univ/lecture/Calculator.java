b976a1d3bd42487feec62ac87fe7e55bb3391701
GaYeon Kim <victoria1559@naver.com>
2017-04-06 21:27:42 +0900

    make num_case in calculator()

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -8,7 +8,27 @@ public class Calculator {
         return calculator(exp);
     }
 
-    public int calculator(String postfix){}
+    public int calculator(String postfix){
+    	double a, b;
+        String temp = new String();
+        Stack stack = new Stack();
+        for (int i = 0; i < postfix.length(); i++) {
+           switch (postfix.charAt(i)) { // 숫자가 나올 때마다 temp에 이어붙임
+           case '0':
+           case '1':
+           case '2':
+           case '3':
+           case '4':
+           case '5':
+           case '6':
+           case '7':
+           case '8':
+           case '9':
+           case '.':
+              temp = temp.concat(postfix.charAt(i) + "");
+              break;
+           
+    }
     public String postfix(String infix){}
     public int precedence(char c){}
 }
