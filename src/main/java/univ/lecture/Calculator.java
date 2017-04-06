c5b1382c2cfdd13748d66d912e9ad10d2d488520
GaYeon Kim <victoria1559@naver.com>
2017-04-06 21:20:58 +0900

    make method

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -7,4 +7,8 @@ public class Calculator {
     public int calculate(String exp) {
         return 2;
     }
+
+    public int calculator(String postfix){}
+    public String postfix(String infix){}
+    public int precedence(char c){}
 }
