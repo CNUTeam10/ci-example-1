75c4b2c48e0d32a6715676712daf1d24ac67770a
GaYeon Kim <victoria1559@naver.com>
2017-04-06 21:31:36 +0900

    make *_case in calculator()

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -46,6 +46,12 @@ public class Calculator {
 				b = (double) stack.pop();
 				stack.push(b - a);
 				break;
+			case '*': // *가 나오면 double형으로 두 번째 pop한 것 * 첫 번째 pop한 것 을 하여 결과를
+				// push
+				a = (double) stack.pop();
+				b = (double) stack.pop();
+				stack.push(b * a);
+				break;
 
 
 			}
